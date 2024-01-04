fb520d677dd464995226bc2c52f5fb5325a9cd0e
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-01-04 22:15:38 +0530

    made all the changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,91 @@
-import './App.css'
-
-import { Signin } from './components/Signin'
 import { initializeApp } from "firebase/app";
+import { Signin } from "./components/Signin";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { RecoilRoot, useRecoilState } from "recoil";
+import { userAtom } from "./store/atoms/user";
+import { Topbar } from "./components/Topbar";
+import { ProblemList } from "./components/ProblemList";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { About } from "./components/About";
+import { Landing } from "./components/Landing";
+import { Submissions } from "./components/Sumissions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAp6wRft8cx49zgzIHv8x-lb_vmlh7rVRM",
-  authDomain: "leetcode-c8c1e.firebaseapp.com",
-  projectId: "leetcode-c8c1e",
-  storageBucket: "leetcode-c8c1e.appspot.com",
-  messagingSenderId: "703177137670",
-  appId: "1:703177137670:web:cdef242f0f78fd9536ed87",
-  measurementId: "G-S8LKZGKX02"
+  apiKey: "AIzaSyAjjsbl9eSDWSmfrWpFPap2uGuwONZ2N4g",
+  authDomain: "leetcode-clone-c39eb.firebaseapp.com",
+  projectId: "leetcode-clone-c39eb",
+  storageBucket: "leetcode-clone-c39eb.appspot.com",
+  messagingSenderId: "66814187798",
+  appId: "1:66814187798:web:a6b3702e191448722dd837",
+  measurementId: "G-ET5FNB5WCN"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 function App() {
   return (
-    <>
-    <Signin />
-    </>
-  )
+    <RecoilRoot>
+      <StoreApp />
+    </RecoilRoot>
+  );
 }
 
-export default App
+function StoreApp() {
+  const [user, setUser] = useRecoilState(userAtom);
+
+  const problemList = [
+    { id: "1", problemName: "Two Sum", tags: ["Array", "Hash Table"] },
+    { id: "2", problemName: "Reverse String", tags: ["String"] },
+    { id: "3", problemName: "Palindrome Check", tags: ["String"] },
+    { id: "4", problemName: "Merge Intervals", tags: ["Array", "Sorting"] },
+  ];
+
+  useEffect(() => {
+    onAuthStateChanged(auth, function (user) {
+      if (user && user.email) {
+        setUser({
+          loading: false,
+          user: {
+            email: user.email,
+          },
+        });
+      } else {
+        setUser({
+          loading: false,
+        });
+        // No user is signed in.
+        console.log("There is no logged in user");
+      }
+    });
+  }, [])
+
+  if (user.loading) {
+    return <div>loading ...</div>;
+  }
+  
+  if (!user.user) {
+    return <div><Signin /></div>
+  }
+  
+  return (
+    <div className="place-items-center grid">
+      <div className="max-w-screen-lg w-full">
+      <Router>
+        <Topbar />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/activity" element={<Submissions />} />
+            <Route path="/problems" element={<ProblemList problemList={problemList} />} />
+          </Routes>
+        </Router>
+        {/* <Leaderboard /> */}
+        {/* <Leaderboard leaderboard={leaderboardData} /> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/components/Signin.tsx
+++ frontend/src/components/Signin.tsx
@@ -1,48 +1,86 @@
-import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
-import { useState } from "react";
-import {app} from "../utils/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { auth } from "../App";
+import { GoogleAuthProvider } from "firebase/auth";
+import GoogleIcon from "../assets/google.svg";
+import GithubIcon from "../assets/github.svg";
 
-const actionCodeSettings = {
-  // URL you want to redirect back to. The domain (www.example.com) for this
-  // URL must be in the authorized domains list in the Firebase Console.
-  url: 'https://localhost:5173',
-  // This must be true.
-  handleCodeInApp: true,
-};
-
+const provider = new GoogleAuthProvider();
 
 export const Signin = () => {
 
-const auth = getAuth(app);
+    async function onSignin() {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                if (!credential) {
+                    return;
+                }
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user);
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch(() => {
+                alert("erorr while signing in");
+            });
 
-const [email, setEmail] = useState("");
 
-  async function onSignin() {
-    await sendSignInLinkToEmail(auth, email, actionCodeSettings)
-  .then(() => {
-    // The link was successfully sent. Inform the user.
-    // Save the email locally so you don't need to ask the user for it again
-    // if they open the link on the same device.
-    window.localStorage.setItem('emailForSignIn', email);
-    alert("Email sent");
-    // ...
-  })
-  .catch((error) => {
-    alert("Error");
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ...
-  });
-  }
+    }
 
-  return <div>
-    <input type = "text" placeholder="email" onChange={(e) => {
-      setEmail(e.target.value);
-    }}></input>
-    <button onClick = { () => {
-        onSignin()
-    }}> 
-      Signup
-    </button>
-  </div>
+    return (
+        <div className="flex bg-black">
+
+            <div className="w-full md:w-2/5 bg-black flex justify-center items-center h-screen max-sm:hidden max-md:hidden">
+                <div>
+                    <h1 className="text-4xl font-bold mb-4 text-white">Daily Code</h1>
+                    <div className="grid grid-cols-3 gap-4">
+                        {[...Array(9)].map((_, index) => (
+                            <div key={index} className="opacity-50">
+                                <i className="fas fa-arrow-down fa-3x"></i>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="w-full h-screen md:w-3/5 bg-gray-900 flex justify-center items-center">
+                <div className="w-full max-w-md">
+                    <div className="p-5">
+                        <h2 className="text-2xl font-semibold mb-2 text-white text-center">Log In</h2>
+
+                    </div>
+                    <div className=' mb-4  justify-center py-1 sm:px-6 lg:px-8 '>
+                        <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+                            <div className='bg-white py-12 px-4 shadow sm:rounded-lg sm:px-10'>
+                                <div className='flex flex-col items-center justify-center gap-4'>
+                                    <p className='font-normal text-2xl text-gray-900'>Welcome</p>
+
+                                    <p className='font-light text-sm text-gray-600'>
+                                        Log in to continue to DailyCode.
+                                    </p>
+                                    <button
+                                        type='submit'
+                                        className='w-full flex justify-center items-center gap-2 py-3 px-4 border rounded font-light text-md hover:bg-gray-200 focus:outline-none focus:ring-2 '
+                                        onClick={() => onSignin()}
+                                    >
+                                        <img src={GoogleIcon} className='w-5 h-5 mr-2' />
+                                        Continue with Google
+                                    </button>
+                                    <button
+                                        className='w-full flex justify-center items-center gap-2 py-3 px-4 border rounded font-light text-md hover:bg-gray-200 focus:outline-none focus:ring-2 -mt-2'
+                                    >
+                                        <img src={GithubIcon} className='w-5 h-5 mr-2' />
+                                        Continue with Github
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 text-center">
+                            <p className="text-gray-400">New to Neon? <a href="#" className="text-blue-500">Sign up for an account</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
